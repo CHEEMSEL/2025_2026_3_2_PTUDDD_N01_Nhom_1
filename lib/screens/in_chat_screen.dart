09b529cfa1952994f3d866058a50abad1fb5fb4d
mine1x <sktm2k6@gmail.com>
2026-07-14 14:01:40 +0700

    tạo các màn hình trống và đặt màn hình login làm mặc định

---
+++ lib/screens/in_chat_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InChatScreen extends StatelessWidget {
+  const InChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('In Chat')),
+      body: const Center(child: Text('In Chat')),
+    );
+  }
+}
