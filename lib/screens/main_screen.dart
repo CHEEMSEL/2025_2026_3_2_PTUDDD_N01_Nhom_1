70f0b439947c210ed1c15eb87febfdb9d121da50
CHEEMSEL <24100455@st.phenikaa-uni.edu.vn>
2026-07-22 19:22:31 +0700

    update(main_screen.dart): Thêm bottom nav bar (4 tabs)

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,65 @@
+import "package:flutter/material.dart";
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int slectedIndex = 0;
+  //Các tabs
+  final List<Widget> _tabs = [
+    Center(child: Text("Home Tab")),
+    Center(child: Text("Contact Tab")),
+    Center(child: Text("Notifications Tab")),
+    Center(child: Text("Settings Tab"))
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main Screen"),
+      ),
+      body: IndexedStack(
+        index: slectedIndex,
+        children: _tabs,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        //Chọn kiểu điều hướng (fixed: hiển thị tất cả các tab, shifting: chỉ hiển thị tab được chọn).
+        //Nếu để shift thì phải đặt màu riêng cho từng item (custom later)
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
+        // Hàm chọn tab
+        currentIndex: slectedIndex,
+        onTap: (index) {
+          setState(() {
+            slectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_pin),
+            label: 'Contact',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
